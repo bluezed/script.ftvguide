3c8ce86b68b611b8d85487a16950e3eb27d44f3a
bluezed <tgeppert@gmail.com>
2015-02-24 20:33:32 +0100

    [v2.0.12] - Guide type selection less hard coded now

--- guideTypes.py
+++ guideTypes.py
@@ -32,6 +32,9 @@ guideTypes = [(30126, 'guide.xmltv'), (30127, 'guide_basic.xmltv'), (30133, 'gui
 def getGuideFileName(id):
     return guideTypes[id][1]
 
+def getGuideName(id):
+    return strings(guideTypes[id][0])
+
 if __name__ == '__main__':
     list = []
     for id, file in guideTypes:
@@ -39,4 +42,4 @@ if __name__ == '__main__':
     d = xbmcgui.Dialog()
     ret = d.select('Select what type of guide you want to use', list)
     ADDON.setSetting('xmltv.type', str(ret))
-    ADDON.setSetting('xmltv.type_select', strings(guideTypes[ret][0]))
+    ADDON.setSetting('xmltv.type_select', getGuideName(ret))
